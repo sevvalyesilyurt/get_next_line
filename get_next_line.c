785fe72fc805700767018af94d11af5320ba6112
sevvalYesilyurt41 <sevvalyesilyurt4@gmail.com>
2026-03-11 21:28:47 +0300

    stash clear

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: sevyesil <sevyesil@student.42kocaeli.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/07 19:57:19 by sevyesil          #+#    #+#             */
-/*   Updated: 2026/03/07 21:32:45 by sevyesil         ###   ########.fr       */
+/*   Updated: 2026/03/11 21:20:31 by sevyesil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,19 +34,22 @@ static char	*get_read_file(int fd, char *buffer)
 	int		byte_size;
 
 	byte_size = 1;
+	if (!buffer)
+		buffer = (char *)calloc(1,1);
 	temp_buffer = (char *)malloc(BUFFER_SIZE + 1);
-	while (byte_size > 0 && ft_strchr(buffer, '\n') != 0)
+	while (byte_size > 0 && ft_strchr(buffer, '\n') == 0)
 	{
-		byte_size = read(fd, temp_buffer, BUFFER_SIZE);
+		byte_size = read(fd, buffer, BUFFER_SIZE);
 		if (byte_size == -1)
 		{
+			free(buffer);
 			free(temp_buffer);
 			return (NULL);
 		}
 		temp_buffer[byte_size] = '\0';
 		buffer = get_free(buffer, temp_buffer);
-		free(temp_buffer);
 	}
+	free(temp_buffer);
 	return (buffer);
 }
 
@@ -58,30 +61,46 @@ static char	*get_line(char *buffer)
 
 	i = 0;
 	length = 0;
-	next_buffer = (char *)malloc(ft_strlen(buffer));
-	while (buffer[i])
-	{
-		if (buffer[i] != '\n')
-		{
-			length++;
-		}
+	if (!buffer || buffer[0] == '\0')
+		return (NULL);
+	while (buffer[i] && buffer[i] != '\n')
 		i++;
-	}
-	next_buffer = (char *)malloc(length);
+	if (buffer[i] == '\n')
+		i++;
 	next_buffer = ft_substr(buffer, 0, length);
 	return (next_buffer);
+}
+
+static char	*get_stash_clear(char *buffer)
+{
+	int		i;
+	char	*next_buffer;
+
+	i = 0;
+	while (buffer[i] && buffer[i] != '\n')
+		i++;
+	if (buffer[i] == '\n')
+	{
+		next_buffer = ft_substr(buffer, 0, i);
+		free(buffer);
+		return (next_buffer);
+	}
+	free(buffer);
+	buffer = NULL;
+	return (buffer);
 }
 
 char	*get_next_line(int fd)
 {
 	static char	*buffer;
-	static char	*new_buffer;
+	static char	*line;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	buffer = get_read_file(fd, buffer);
-	new_buffer = get_line(buffer);
-	return (new_buffer);
+	line = get_line(buffer);
+	buffer = get_stash_clear(buffer);
+	return (line);
 }
 #include <stdio.h>
 
